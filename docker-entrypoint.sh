0d6cc739591dde83657f2e2fc2ea7dd461cbdcad
root <root@cloud.christian-f-brinkmann.de>
2024-10-24 17:32:02 +0200

    copy default config if config dir is empty, change program call to switch user as the entry point script is now run as root

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/sh
+#set -e
 
-java -jar -Dnogui=true /jmb/JMusicBot.jar
+
+# Check if the config directory is empty
+if [ -z "$(ls -A /jmb/config)" ]; then
+    echo "Config directory is empty. Copying default config..."
+    cp /jmb/reference/config.txt /jmb/config/config.txt
+fi
+
+chmod -R 755 /jmb/config && chown -R 10000:10001 /jmb/config
+
+exec su-exec appuser java -jar -Dnogui=true /jmb/JMusicBot.jar
